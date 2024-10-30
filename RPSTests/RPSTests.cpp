b4de7a88c59c474ad1965335e72ed7616a789405
Harsh Nimje <harshnimje786@gmail.com>
2024-10-31 00:12:33 +0530

    Revert "Precompiled headers error solved"
    This reverts commit 41a5abab21aed240905fc8a2c2a3951f97f3eca3.

--- RPSTests/RPSTests.cpp
+++ RPSTests/RPSTests.cpp
@@ -1,5 +1,5 @@
-#include "pch.h"
 #include "CppUnitTest.h"
+#include "pch.h"
 extern "C" {
 #include "../RockPaperScissors/RPS.c"  
 }
--- RockPaperScissors/RPS.c
+++ RockPaperScissors/RPS.c
@@ -1,7 +1,6 @@
-#include "../RPSTests/pch.h"
 #include <stdio.h>
 #include <string.h>
-
+#include "../RPSTests/pch.h"
 const char* determineWinner(const char* player1, const char* player2) {
     if ((strcmp(player1, "Rock") != 0 && strcmp(player1, "Paper") != 0 && strcmp(player1, "Scissors") != 0) ||
         (strcmp(player2, "Rock") != 0 && strcmp(player2, "Paper") != 0 && strcmp(player2, "Scissors") != 0)) {
