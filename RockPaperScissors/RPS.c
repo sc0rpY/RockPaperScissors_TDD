f493643862d64f777297320b3596cfab8ae92e6c
Harsh Nimje <harshnimje786@gmail.com>
2024-10-30 20:28:34 +0530

    Add initial Rock,Paper and Scissors Logic

---
+++ RockPaperScissors/RPS.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+const char* determineWinner(const char* player1, const char* player2) {
+    if ((strcmp(player1, "Rock") != 0 && strcmp(player1, "Paper") != 0 && strcmp(player1, "Scissors") != 0) ||
+        (strcmp(player2, "Rock") != 0 && strcmp(player2, "Paper") != 0 && strcmp(player2, "Scissors") != 0)) {
+        return "Invalid";
+    }
+    if (strcmp(player1, player2) == 0) {
+        return "Draw";
+    }
+    if ((strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0) ||
+        (strcmp(player1, "Scissors") == 0 && strcmp(player2, "Paper") == 0) ||
+        (strcmp(player1, "Paper") == 0 && strcmp(player2, "Rock") == 0)) {
+        return "Player1";
+    }
+    return "Player2";
+}
